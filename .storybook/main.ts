f0c323d0216f19730b2103913d47cab35d764c58
Pavel Laptev <pawellaptew@gmail.com>
2026-05-10 01:35:58 +0200

    bump dep version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
+import type { Configuration } from 'webpack'
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -16,6 +18,29 @@ const config: StorybookConfig = {
   },
   core: {
     builder: '@storybook/builder-webpack5'
+  },
+  webpackFinal: async (config: Configuration) => {
+    config.module = config.module ?? { rules: [] }
+    config.module.rules = config.module.rules ?? []
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: { node: 'current' } }],
+              ['@babel/preset-react', { runtime: 'automatic' }],
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      ]
+    })
+    config.resolve = config.resolve ?? {}
+    config.resolve.extensions = [...(config.resolve.extensions ?? []), '.ts', '.tsx']
+    return config
   }
 }
 export default config
